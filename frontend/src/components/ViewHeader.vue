cc340ef1b3b3d30de9cb07d304db637c8879d276
Nick-2002b <nicolae.balaban@studio.unibo.it>
2026-05-25 01:29:44 +0200

    feat: add ViewHeader component and integrate it into Agenda page with button functionality

---
+++ frontend/src/components/ViewHeader.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  buttonText?: string;
+}>();
+
+defineEmits<{
+  (e: "action"): void;
+}>();
+</script>
+<template>
+  <div class="-mx-8 border-b border-gray-200 px-8 py-6">
+    <div class="flex items-center justify-between">
+      <h1 class="text-3xl font-medium">{{ title }}</h1>
+
+      <div class="flex items-center gap-2">
+        <button
+          v-if="buttonText"
+          @click="$emit('action')"
+          class="px-4 py-2 border border-gray-200 bg-primary hover:bg-primary/70 font-medium rounded-full shadow-sm flex items-center gap-2 cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M5 12h14" />
+            <path d="M12 5v14" />
+          </svg>
+          {{ buttonText }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
